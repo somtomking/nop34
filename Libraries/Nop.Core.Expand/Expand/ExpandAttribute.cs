c98ed594360ef389fa0c0f2bb6a958a429b2a360
somtomking <somtomking@163.com>
2014-10-16 16:13:43 +0800

    添加新的工程（nop拓展）

---
+++ Libraries/Nop.Core.Expand/Expand/ExpandAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Expand
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)]
+    public sealed class ExpandAttribute : Attribute
+    {
+        public ExpandType Type { get; private set; }
+        public string Desc { get; private set; }
+        public ExpandAttribute(ExpandType expandType, string desc)
+        {
+            this.Desc = desc;
+            this.Type = expandType;
+        }
+    }
+
+    public enum ExpandType
+    {
+        New,
+        Update
+    }
+
+}
