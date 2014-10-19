d8c173d6c00d50e6bece516e16bc9568e7f10519
somtomking <somtomking@163.com>
2014-10-19 20:27:04 +0800

    添加运行的信息

---
+++ Presentation/Nop.Web/Administration/Controllers/CommonExpandController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nop.Core.Expand;
+using Nop.Admin.Models.CommonExpand;
+namespace Nop.Admin.Controllers
+{
+    [Expand(ExpandType.New, "对CommonController的拓展")]
+    public partial class CommonExpandController : BaseAdminController
+    {
+        public ActionResult RunInfo()
+        {
+            RunInfoModel model = new RunInfoModel();
+            model.RAM = (((double)System.Diagnostics.Process.GetCurrentProcess().WorkingSet64) / 1024) / 1024;
+            return View(model);
+        }
+    }
+}
---
+++ Presentation/Nop.Web/Administration/Models/CommonExpand/RunInfoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nop.Admin.Models.CommonExpand
+{
+    public class RunInfoModel
+    {
+        public double RAM { get; set; }
+    }
+}
