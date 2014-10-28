e8cc54a134d020ace7ea76c8e1a0d8fa76fddc10
somtomking <somtomking@163.com>
2014-10-28 22:49:02 +0800

    添加城市和区县的类

---
+++ Libraries/Nop.Core/Domain/Directory/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Expand;
+namespace Nop.Core.Domain.Directory
+{
+    [Expand(ExpandType.New)]
+    public partial class City : BaseEntity
+    {
+        public int ProvinceId { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public bool Published { get; set; }
+        public int DisplayOrder { get; set; }
+        public virtual StateProvince Province { get; set; }
+    }
+}
