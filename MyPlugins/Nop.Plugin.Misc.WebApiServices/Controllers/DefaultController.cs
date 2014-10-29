f0a9d495200089012e6c58751ab6219a1c5c0fa3
somtomking <somtomking@163.com>
2014-10-29 21:23:15 +0800

    api ok~

--- MyPlugins/Nop.Plugin.Misc.WebApiServices/Controllers/DefaultController.cs
+++ MyPlugins/Nop.Plugin.Misc.WebApiServices/Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nop.Core.Domain.Common;
+using Nop.Services.Orders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,10 +11,18 @@ namespace Nop.Plugin.Misc.WebApiServices.Controllers
 {
     public class DefaultController : ApiController
     {
+        private readonly IOrderService _orderService;
+        private readonly AdminAreaSettings _adminAreaSettings;
+        public DefaultController(IOrderService orderService, AdminAreaSettings adminAreaSettings)
+        {
+            _orderService = orderService;
+            _adminAreaSettings = adminAreaSettings;
+
+        }
         // GET: api/Default
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", _adminAreaSettings.GridPageSizes };
         }
 
         // GET: api/Default/5
